4d8f45d58fde7d9655aacae35caf437e2f58f602
Sam Webster <sam@secondhandlegends.com>
2019-12-06 15:53:49 -0500

    Setting up project in version control

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  
 
+  
+  render() {
+    return (
+      <div>
+        <div className="background">
+          
+        <div className="main-content">
+          <h1 className="title">
+            Fauxetta Stone
+          </h1>
+          <p className="description">
+            Specify the number of paragraphs of text you would like to generate. All content is generated from Greek and Roman texts.
+          </p>
+  
+      </div>
+      </div>
+    </div>
+    );
+  }
+}
 export default App;
